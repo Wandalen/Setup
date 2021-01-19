14642bc16f369dcda202e848c698f28c486948f2
Dmytro <dm.vict.kr@gmail.com>
2021-01-19 09:38:56 +0200

    .

--- proto/GitConfigGlobalSetup.bat
+++ proto/GitConfigGlobalSetup.bat
@@ -8,13 +8,13 @@ git config --global core.ignorecase false
 git config --global core.fileMode false
 git config --global credential.helper store
 
-if not %NAME% == "" (
+if not "%NAME%" == "" (
   git config --global user.name "%NAME%"
   git config --global url."https://%NAME%@github.com".insteadOf "https://github.com"
   git config --global url."https://%NAME%@bitbucket.org".insteadOf "https://bitbucket.org"
 )
 
-if not %EMAIL% == "" (
+if not "%EMAIL%" == "" (
   git config --global user.email "%EMAIL%"
 )
 
