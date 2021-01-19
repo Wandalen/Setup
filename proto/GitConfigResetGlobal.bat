f979a07e5b5512823aab5ab79b7c8963e05a44e3
Dmytro <dm.vict.kr@gmail.com>
2021-01-19 08:07:58 +0200

    Extend scripts `GitConfigResetGlobal*`, add conditions in scripts

---
+++ proto/GitConfigResetGlobal.bat
@@ -0,0 +1,20 @@
+@echo off
+
+set NAME=%1
+set EMAIL=%2
+
+git config --global core.autocrlf false
+git config --global core.ignorecase false
+git config --global core.fileMode false
+git config --global credential.helper store
+
+if not %NAME% == "" (
+  git config --global user.name "%NAME%"
+  git config --global url."https://%NAME%@github.com".insteadOf "https://github.com"
+  git config --global url."https://%NAME%@bitbucket.org".insteadOf "https://bitbucket.org"
+)
+
+if not %EMAIL% == "" (
+  git config --global user.email "%EMAIL%"
+)
+
