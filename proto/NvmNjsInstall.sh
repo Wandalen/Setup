aefa0cc8e3a6a1f0a910d7d197c005bc0328d5d2
Dmytro <dm.vict.kr@gmail.com>
2021-01-20 10:11:11 +0200

    Update script `NvmNjsInstall.sh`

--- proto/NvmNjsInstall.sh
+++ proto/NvmNjsInstall.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
+VERSION=$1
 NVM_DIR="$HOME/.nvm"
 
-cd "$HOME"
-if [ ! -d ".nvm" ] ; then
-  mkdir ".nvm"
+if [ ! -d $NVM_DIR ] ; then
+  mkdir $NVM_DIR
 fi
 
 if command -v wget &> /dev/null
@@ -25,7 +25,8 @@ PID=$(pgrep "install.sh")
 wait $PID
 
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-nvm install --lts --latest-npm
+
+nvm install --lts --latest-npm "$VERSION"
 
 exit 0
 
