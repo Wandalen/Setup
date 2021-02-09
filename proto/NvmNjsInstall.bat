b8104bfd0857dfcdd2e59282248e0567b0f94203
Dmytro <dm.vict.kr@gmail.com>
2021-02-09 18:21:25 +0200

    Fix style problem

--- proto/NvmNjsInstall.bat
+++ proto/NvmNjsInstall.bat
@@ -1,20 +1,18 @@
 @echo off
 
 where choco.exe
-if %ERRORLEVEL% NEQ 0(
+if %ERRORLEVEL% NEQ 0 (
   REM  --> Check for permissions
   if "%PROCESSOR_ARCHITECTURE%" EQU "amd64" (
     >nul 2>&1 "%SYSTEMROOT%\SysWOW64\cacls.exe" "%SYSTEMROOT%\SysWOW64\config\system"
-  )
-  else (
+  ) else (
     >nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
   )
 
   REM --> If error flag set, we do not have admin.
   if '%errorlevel%' NEQ '0' (
     goto UACPrompt
-  )
-  else (
+  ) else (
     goto gotAdmin
   )
 
@@ -30,14 +28,12 @@ if %ERRORLEVEL% NEQ 0(
   pushd "%CD%"
   CD /D "%~dp0"
 
-
   echo "Program Chocolatey not found."
   set /P ANSWER="Do you want to install Chocolatey now? (y/n)"
   if  %ANSWER% == "y" OR %ANSWER% == "Y" (
     @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "[System.Net.ServicePointManager]::SecurityProtocol = 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
   )
 )
-
 
 set VERSION=%1
 
