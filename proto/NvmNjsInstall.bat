03b3f22d4d60aee59005dbd70e6540a68892f754
dmvict <dm.vict.kr@gmail.com>
2021-02-26 11:19:30 +0200

    Experiment

--- proto/NvmNjsInstall.bat
+++ proto/NvmNjsInstall.bat
@@ -4,8 +4,7 @@ where choco.exe
 if %ERRORLEVEL% NEQ 0 (
   echo Please, install the utility Chocolatey and then run the script
   echo Run next command in administrative shell to install utility :
-  echo @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "[System.Net.ServicePointManager]::SecurityProtocol = 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
-  exit /b 
+  exit /b
 )
 
 set VERSION=%1
