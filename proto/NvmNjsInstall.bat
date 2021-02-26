3993a96277d66013f45786e623a459469fe8f107
dmvict <dm.vict.kr@gmail.com>
2021-02-26 10:26:24 -0800

    Improve script `NvmNjsInstall.bat`, add restrictions and explanations

--- proto/NvmNjsInstall.bat
+++ proto/NvmNjsInstall.bat
@@ -2,37 +2,10 @@
 
 where choco.exe
 if %ERRORLEVEL% NEQ 0 (
-  REM  --> Check for permissions
-  if "%PROCESSOR_ARCHITECTURE%" EQU "amd64" (
-    >nul 2>&1 "%SYSTEMROOT%\SysWOW64\cacls.exe" "%SYSTEMROOT%\SysWOW64\config\system"
-  ) else (
-    >nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
-  )
-
-  REM --> If error flag set, we do not have admin.
-  if '%errorlevel%' NEQ '0' (
-    goto UACPrompt
-  ) else (
-    goto gotAdmin
-  )
-
-  :UACPrompt
-  echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
-  set params= %*
-  echo UAC.ShellExecute "cmd.exe", "/c ""%~s0"" %params:"=""%", "", "runas", 1 >> "%temp%\getadmin.vbs"
-  "%temp%\getadmin.vbs"
-  del "%temp%\getadmin.vbs"
-  exit /B
-
-  :gotAdmin
-  pushd "%CD%"
-  CD /D "%~dp0"
-
-  echo "Program Chocolatey not found."
-  set /P ANSWER="Do you want to install Chocolatey now? (y/n)"
-  if  %ANSWER% == "y" OR %ANSWER% == "Y" (
-    @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "[System.Net.ServicePointManager]::SecurityProtocol = 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
-  )
+  echo Please, install the utility Chocolatey and then run the script
+  echo Run next command in administrative shell to install utility :
+  echo @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "[System.Net.ServicePointManager]::SecurityProtocol = 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
+  exit /b 
 )
 
 set VERSION=%1
